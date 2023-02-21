6a8b0dbf9fe544f0991d8ed9f6beb5d30b1dacae
kevinhouguet <k.houguet@gmail.com>
2023-02-21 17:31:50 +0100

    test

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
-const baseUrl = process.env.NODE_ENV === 'prod' ? process.env.BASE_URL_PROD : process.env.BASE_URL_DEV;
+const baseUrl = process.env.NODE_ENV === 'production' ? process.env.BASE_URL_PROD : process.env.BASE_URL_DEV;
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
