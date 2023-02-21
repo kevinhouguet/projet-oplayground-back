52b490b8fd18e29de501d519d23589a57d54cda3
kevinhouguet <k.houguet@gmail.com>
2023-02-21 17:09:57 +0100

    test

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const routers = require('./routers');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT;
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
