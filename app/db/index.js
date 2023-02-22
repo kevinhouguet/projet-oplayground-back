de706180aa757609898d10566f8dc856b93166e6
kevinhouguet <k.houguet@gmail.com>
2023-02-22 18:48:23 +0100

    fix: connexion db sqitch OK

--- app/db/index.js
+++ app/db/index.js
@@ -9,7 +9,11 @@ if (process.env.NODE_ENV === 'production') {
     ssl: true,
   });
 } else {
-  client = new Client();
+  // client = new Client();
+  client = new Client({
+    connectionString: 'postgres://mpqwdzniajshyx:614201553d71c41ae1a7c42fc5da960fe6fdd57a833f982072413f4081491652@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/db522t62llgr1q',
+    ssl: true,
+  });
 }
 console.log(process.env.DATABASE_URL);
 console.log(process.env.NODE_ENV);
