1b1ca61dc89382d34dbd7497431c5a1580893b10
kevinhouguet <k.houguet@gmail.com>
2023-02-22 17:28:48 +0100

    test

--- app/db/index.js
+++ app/db/index.js
@@ -5,7 +5,8 @@ let client;
 
 if (process.env.NODE_ENV === 'production') {
   client = new Client({
-    connectionString: `${process.env.DATABASE_URL}?sslmode=require`,
+    connectionString: `${process.env.DATABASE_URL}`,
+    ssl: true,
   });
 } else {
   client = new Client();
