aa00d2180ed43a5319ddac805a965ae9d4ec7493
Adim Zribi <zribiadim@gmail.com>
2023-02-22 13:51:43 +0100

    add insert encounter and playground to sql

--- migrations/deploy/add_functions.sql
+++ migrations/deploy/add_functions.sql
@@ -29,5 +29,62 @@ MEMBER AS $$
 	RETURNING *;
 	$$ LANGUAGE 
 SQL; 
+CREATE FUNCTION "INSERTPLAYGROUND"(playground JSON) RETURNS 
+playground AS $$ 
+	INSERT INTO
+	    "playground" (
+	        "name",
+	        "address",
+	        "zip_code",
+	        "city",
+	        "surface",
+	        "picture"
+	    )
+	VALUES (
+	        playground ->> 'name',
+	        playground ->> 'address',
+	        playground ->> 'zip_code',
+	        playground ->> 'city',
+	        playground ->> 'surface',
+			playground ->> 'picture'
+ 
+	    )
+	RETURNING *;
+	$$ LANGUAGE 
+SQL; 
+
+
+
+
+CREATE FUNCTION "INSERTENCOUNTER"(encounter JSON) RETURNS 
+encounter AS $$ 
+	INSERT INTO
+	    "encoutner" (
+	        "name",
+			"member_id",
+	        "start_date",
+	        "stop_date",
+	        "max-player",
+	        "playground",
+	        "created_at", 
+			"updated_at"
+	    )
+	VALUES (
+	        playground ->> 'name',
+			playground ->> 'member_id'
+	        playground ->> 'start_date',
+	        playground ->> 'stop_date',
+	        playground ->> 'max_player',
+	        playground ->> 'playground',
+			playground ->> 'created_ad',
+			playground ->> 'updated_at'
+			
+ 
+	    )
+	RETURNING *;
+	$$ LANGUAGE 
+SQL; 
+
+
 
 COMMIT;
