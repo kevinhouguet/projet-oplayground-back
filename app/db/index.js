cccf510f340607a78be7bc1bee96c1da856524de
kevinhouguet <k.houguet@gmail.com>
2023-02-22 16:37:40 +0100

    fix: attempt to fix connexion db

--- app/db/index.js
+++ app/db/index.js
@@ -6,8 +6,12 @@ let client;
 if (process.env.NODE_ENV === 'production') {
   client = new Client();
 } else {
-  client = new Client(process.env.DATABASE_URL);
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
 }
+console.log(process.env.DATABASE_URL);
+console.log(process.env.NODE_ENV);
 
 client.connect()
   .then(() => console.log('DB Connexion : OK'))
