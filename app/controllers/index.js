3dada90aa4a2319abed6b9f290e32c51211ccf86
kevinhouguet <k.houguet@gmail.com>
2023-02-23 19:00:16 +0100

    :hammer: refacto: call of controllerWrapper

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,9 @@
 const terrainsController = require('./api/terrainsController');
 const usersController = require('./api/usersController');
+const controllerWrapper = require('./api/controllerWrapper');
 
 module.exports = {
   terrainsController,
   usersController,
+  controllerWrapper
 };
