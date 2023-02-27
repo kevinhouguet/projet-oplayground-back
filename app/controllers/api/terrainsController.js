44d72d44b2d0934a6824680b5254e67d0b991b10
kevinhouguet <k.houguet@gmail.com>
2023-02-27 15:35:53 +0100

    :sparkles: feat: update playground List, fetch api and render format playground

--- app/controllers/api/terrainsController.js
+++ app/controllers/api/terrainsController.js
@@ -1,15 +1,29 @@
 module.exports = {
 
-  playgroundList: (req, res) => {
-    res.json({ message: 'salut noob' });
-    // throw new Error('salut');
+  playgroundList: async (req, res) => {
+    const { inseeCode } = req.params;
+    const url = `https://equipements.sports.gouv.fr/api/records/1.0/search/?dataset=data-es&q=codeinsee%3D${inseeCode}`;
+    const httpResponse = await fetch(url);
+    const data = await httpResponse.json();
+
+    const renderData = data.records.map((element) => ({
+      name: element.fields.nominstallation,
+      surface: element.fields.caract167,
+      address: element.fields.adresse,
+      zipCode: element.fields.codepostal,
+      city: element.fields.commune,
+    }));
+    // console.log(renderData);
+
+    // res.json(data.records[0].fields.nominstallation);
+    res.json(renderData);
   },
 
-  playgroundById: (req, res) => {
-    res.json({ message: 'salut terrain' });
-  },
+  // playgroundById: (req, res) => {
+  //   res.json({ message: 'voici les infos sur le terrain demandé' });
+  // },
 
   playgroundEvent: (req, res) => {
-    res.json({ message: 'bienvenue dans mon event' });
+    res.json({ message: 'bienvenue dans mes events' });
   },
 };
--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const routers = require('./routers');
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 require('dotenv').config();
 
 const app = express();
