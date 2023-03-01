958a2d0107e30bbd0108a1b89bc27e21cf80bec5
kevinhouguet <k.houguet@gmail.com>
2023-03-01 14:29:36 +0100

    add some schema for swagger

---
+++ app/swagger/schemas/events.js
@@ -0,0 +1,25 @@
+/**
+ *  @swagger
+ *  components:
+ *    schemas:
+ *      Event:
+ *        properties:
+ *          id:
+ *            type: integer
+ *          name:
+ *            type: string
+ *          start_date:
+ *            type: string
+ *          stop_date:
+ *            type: string
+ *          max_player:
+ *            type: string
+ *          member_id:
+ *            type: string
+ *          playground_id:
+ *            type: integer
+ *          created_at:
+ *            type: string
+ *          updated_at:
+ *            type: array
+ */
