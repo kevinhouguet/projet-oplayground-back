2dca1e289e6ec6dfdf42ec9a029eae0ed4ee83c7
kevinhouguet <k.houguet@gmail.com>
2023-03-02 20:47:47 +0100

    :bug: fix: issue with email

--- app/validation/schemas/member.schema.js
+++ app/validation/schemas/member.schema.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 require('dotenv').config();
 
-// const emailRule = Joi.string().pattern(/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+.[a-z]+$/);
-const emailRule = Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } });
+const emailRule = Joi.string().pattern(/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+.[a-z]+$/);
+// const emailRule = Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } });
 
 const passwordRule = Joi.string().pattern(new RegExp(process.env.PASSWORD_RULE));
 
