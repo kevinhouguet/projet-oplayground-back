8c2aa7875ee4e8af79d13573fa156786333e1bcf
kevinhouguet <k.houguet@gmail.com>
2023-03-03 14:02:19 +0100

    :sparkles: add conf jsdoc

--- app/errors/ApiError.js
+++ app/errors/ApiError.js
@@ -1,5 +1,11 @@
 const BaseError = require('./BaseError');
 
+/**
+ * class ApiError
+ * @constructor
+ * @param {string} name - the name of the error
+ *
+ */
 class ApiError extends BaseError {
   constructor(name, httpCode, message = 'Internal Server Error') {
     super(name, httpCode, message);
