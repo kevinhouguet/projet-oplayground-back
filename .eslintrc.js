d540a56b2a2d3c5802516c58154ba3833b1e7c61
kevinhouguet <k.houguet@gmail.com>
2023-03-06 17:55:10 +0100

    :sparkles: feat: add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,10 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
