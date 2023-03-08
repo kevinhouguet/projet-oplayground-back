94ce14f37a5c8f6c9e6baa5213752177cdf9b982
kevinhouguet <k.houguet@gmail.com>
2023-03-08 09:46:47 +0100

    change token content

--- app/controllers/api/usersController.js
+++ app/controllers/api/usersController.js
@@ -133,7 +133,6 @@ module.exports = {
     if (match) {
       searchMemberEmail = {
         id: searchMemberEmail.id,
-        username: searchMemberEmail.username,
       };
       // JWT : https://www.youtube.com/watch?v=mbsmsi7l3r4&t=804s
       const accessToken = jwt.sign(searchMemberEmail, process.env.ACCESS_TOKEN_SECRET);
